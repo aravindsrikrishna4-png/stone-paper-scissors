598c2c7657cbb962a3b9c892e95cffdb1dee3685
Aravind Sri Krishna B <aravindsrikrishna4@gmail.com>
2026-08-29 21:55:58 +0530

    Added Stone Paper Scissors game

---
+++ helloworld1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    float a,b;
+    printf("Enter the value of the first number:");
+    scanf("%d",&a);
+  printf("Enter the value of the second number:");
+    scanf("%d",&b);
+    float c;
+    c=a*b;
+    printf("The product of the first and second number is: %f",c);
+    return 0;
+    
+}
